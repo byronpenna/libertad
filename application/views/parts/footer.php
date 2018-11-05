6afccc290e788da0c18c4f7607238d22553fe475
Byron i7 <byronpenna@gmail.com>
2018-11-04 22:55:46 -0600

    cambios

--- application/views/parts/footer.php
+++ application/views/parts/footer.php
@@ -1,7 +1,40 @@
 <div class="row marginNull footer" >
+	<div class="row container">
+		<div class="col-lg-offset-1 col-lg-4 vcenter">
+			<div class="row">
+				<h3 class="tituloMediosSocialesFooter">Medios sociales</h3>
+				<div class="row">
+					<div class="col-lg-2 iconoRedSocial">
+						<i class="fab fa-facebook"></i>
+					</div>
+					<div class="col-lg-10">
+						<a href="#" class="linkRedSocialFooter">
+							/ajedrezAP
+						</a>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-2 iconoRedSocial">
+						<i class="fab fa-instagram"></i>
+					</div>
+					<div class="col-lg-10">
+						<a href="#" class="linkRedSocialFooter">
+							/ajedrezAP
+						</a>
+					</div>
+				</div>	
+			</div>
+		</div><!-- 
+		--><div class="col-lg-7 vcenter" >
+			<div class="row" style="text-align: center">
+				Ajedrez politico todos los derechos reservados 2018
+				<br>
+				Powered by <a href="http://www.ritsasv.com/" style="color: white; font-weight: bold;" target="_blank">Ritsa</a>
+				<br>
+			</div>		
+		</div>
+	</div>
 	
-		Ajedrez politico todos los derechos reservados 2018
-		<br>
-		Powered by Ritsa
-	
+		
+		
 </div>
